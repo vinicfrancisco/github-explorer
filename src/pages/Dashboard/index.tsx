f0f47e2ad33a75b9bb11dce36bedd588bca7c475
vinicfrancisco <vini.ibi@gmail.com>
2020-04-20 15:50:52 -0300

    creating dashboard page structure and style

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,37 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImage from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
   return (
-    <div>
+    <>
+      <img src={logoImage} alt="Github Explorer" />
       <Title>Explore repositórios no Github</Title>
-    </div>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="test">
+          <img
+            src="https://avatars3.githubusercontent.com/u/26771118?s=460&u=2f4ca10a884ed7aa7b106d206cdeccd8892a5d8a&v=4"
+            alt="Profile"
+          />
+
+          <div>
+            <strong>vinicfrancisco/gobarber-web</strong>
+            <p>Alguma descrição para esse repo</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
   );
 };
 
